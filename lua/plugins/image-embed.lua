c395761512b34b4cc5c845489d6d0bf6945fcdcd
Ghost <122468681+mdk4if@users.noreply.github.com>
2025-06-26 03:18:06 +0530

    Delete lua directory

--- lua/plugins/image-embed.lua
+++
@@ -1,5 +0,0 @@
-return {
-  "HakonHarnes/img-clip.nvim",
-  event = "VeryLazy", -- Can be changed to `cmd = "PasteImg"` if you use a specific command
-  opts = {},
-}
--- lua/plugins/oil.lua
+++
@@ -1,6 +0,0 @@
-return {
-  "stevearc/oil.nvim",
-  cmd = "Oil",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  opts = {},
-}
